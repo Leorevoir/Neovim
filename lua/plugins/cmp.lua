0447aeefc98b3a2f0866400e12952ed3d5d625f3
Leorevoir <leo.quinzler@epitech.eu>
2025-05-01 21:08:15 +0200

    add: working LSP

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,124 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-emoji",
+    "hrsh7th/cmp-cmdline",
+  },
+
+--------------------------------
+
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    local kind_icons = {
+      Text = "󰊄", Method = "", Function = "󰡱", Constructor = "",
+      Field = "", Variable = "󱀍", Class = "", Interface = "",
+      Module = "󰕳", Property = "", Unit = "", Value = "",
+      Enum = "", Keyword = "", Snippet = "", Color = "",
+      File = "", Reference = "", Folder = "", EnumMember = "",
+      Constant = "", Struct = "", Event = "", Operator = "",
+      TypeParameter = "",
+    }
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      experimental = {
+        ghost_text = true,
+      },
+      performance = {
+        debounce = 60,
+        fetching_timeout = 200,
+        max_view_entries = 30,
+      },
+--------------------------------
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+--------------------------------
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+--------------------------------
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+      }),
+--------------------------------
+      formatting = {
+        format = function(entry, vim_item)
+          vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind] or "", vim_item.kind)
+          vim_item.menu = ({
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snip]",
+            buffer = "[Buf]",
+            path = "[Path]",
+            emoji = "[Emoji]",
+          })[entry.source.name]
+          return vim_item
+        end,
+        fields = { "kind", "abbr", "menu" },
+      },
+--------------------------------
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "emoji" },
+        { name = "luasnip", keyword_length = 3 },
+        { name = "buffer", keyword_length = 3 },
+        { name = "path", keyword_length = 3 },
+      }),
+      window = {
+        completion = {
+          border = "solid",
+        },
+        documentation = {
+          border = "solid",
+        },
+      },
+    })
+--------------------------------
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+  end,
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,66 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+
+--------------------------------
+
+  opts = function()
+
+    return {
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+
+      on_attach = function(_, bufnr)
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc, silent = true })
+        end
+        map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
+        map('n', 'K', vim.lsp.buf.hover, 'Hover documentation')
+        map('n', 'gr', vim.lsp.buf.references, 'List references')
+        map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename symbol')
+        map('n', '<leader>ca', vim.lsp.buf.code_action, 'Code actions')
+        map('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, 'Format buffer')
+      end,
+
+--------------------------------
+
+      servers = {
+        clangd = {},
+        pyright = {},
+
+--------------------------------
+--- ignore `vim` variable when i edit Neovim dots
+--------------------------------
+
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { 'vim' } },
+              workspace = { library = vim.api.nvim_get_runtime_file('', true) }
+            }
+         }
+        },
+      },
+
+    }
+
+  end,
+
+--------------------------------
+
+  config = function(_, opts)
+    local mason_config = require('mason-lspconfig')
+    local lspconfig = require('lspconfig')
+
+    mason_config.setup({ ensure_installed = vim.tbl_keys(opts.servers), automatic_installation = true })
+    mason_config.setup_handlers({
+      function(server)
+        local cfg = vim.tbl_deep_extend('force', opts, opts.servers[server] or {})
+        lspconfig[server].setup(cfg)
+      end,
+    })
+  end,
+}
