4439b002c84a221a5523452aaf8dee93befeff64
Leorevoir <leo.quinzler@epitech.eu>
2025-05-01 22:19:26 +0200

    feat: add my NixVim keymaps to lua Neovim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,39 @@ map('n', '<Leader><Leader>', ':Telescope find_files<CR>', opts)
 map('n', '<Leader>r/', ':Telescope live_grep search_dirs=/<CR>', opts)
 map('n', '<Leader>rr', ':Telescope live_grep search_dirs=%:p:h<CR>', opts)
 
+--------------------------------
+--- ctrl+s to save (shortcut)
+--------------------------------
+
+map('i', '<C-s>', '<cmd>w<cr><esc>', opts)
+map('n', '<C-s>', '<cmd>w<cr><esc>', opts)
+
+--------------------------------
+--- open partend directory
+--------------------------------
+
+map('n', '-', ':Oil<CR>', opts)
+
+--------------------------------
+--- leader QQ to force quit
+--------------------------------
+
+map('n', '<leader>qq', '<cmd>quitall<cr><esc>', opts)
+
+--------------------------------
+--- ctrl + a to select all
+--------------------------------
+
+map('n', '<C-a>', 'gg<S-v>G', opts)
+
+--------------------------------
+--- open terminal below
+--------------------------------
+
+map('n', '<leader>t', ':sp +:terminal<CR>i', opts)
+
+--------------------------------
+--- escape mode in terminal
+--------------------------------
+
+map('t', '<Esc>', '<C-\\><C-n>', opts)
