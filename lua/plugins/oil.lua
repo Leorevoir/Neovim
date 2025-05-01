069bc2d21390391b143196324ebdf4798d44e01c
Leorevoir <leo.quinzler@epitech.eu>
2025-05-01 23:41:47 +0200

    feat: add oil plugin and `-` autocommand

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  dependencies = { { "rachartier/tiny-devicons-auto-colors.nvim", opts = {} } },
+  lazy = false,
+}
