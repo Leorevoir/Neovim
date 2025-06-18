caf0758b1f54be6633842dd21bb6cc7b578f3faf
Leorevoir <leo.quinzler@epitech.eu>
2025-06-18 16:43:38 +0200

    add: tiny-devicons for GLSL shaders .frag .vert

--- lua/plugins/tiny-devicons-auto-colors.lua
+++ lua/plugins/tiny-devicons-auto-colors.lua
@@ -6,5 +6,19 @@ return {
   event = "VeryLazy",
   config = function()
     require("tiny-devicons-auto-colors").setup()
+    require("nvim-web-devicons").set_icon({
+      frag = {
+        icon = "",
+        color = "#ff9e64",
+        cterm_color = "202",
+        name = "Frag",
+      },
+      vert = {
+        icon = "",
+        color = "#4f5b93",
+        cterm_color = "63",
+        name = "Vert",
+      },
+    })
   end,
 }
